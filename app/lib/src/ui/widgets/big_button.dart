2ae56c6773cac088d673a3c4de29a98f4415a161
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-11-30 16:30:28 +0100

    Add style widgets

---
+++ app/lib/src/ui/widgets/big_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:timetracking/src/ui/themes/themes.dart';
+
+class BigButton extends StatelessWidget {
+  final Color buttonColor;
+  final String textValue;
+  final Color textColor;
+
+  const BigButton(
+      {Key? key,
+      required this.buttonColor,
+      required this.textValue,
+      required this.textColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: BorderRadius.circular(14.0),
+      elevation: 0,
+      child: Container(
+        height: 56,
+        decoration: BoxDecoration(
+          color: buttonColor,
+          borderRadius: BorderRadius.circular(14.0),
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {},
+            borderRadius: BorderRadius.circular(14.0),
+            child: Center(
+              child: Text(
+                textValue,
+                style: heading5.copyWith(color: textColor),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

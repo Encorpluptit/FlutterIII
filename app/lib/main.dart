f2048e6ed591aced0dc341af92a8efb89b909aca
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-12-05 11:59:01 +0100

    Add firebase app initialization

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,31 +1,102 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:timetracking/src/blocs/login/bloc.dart';
-import 'package:timetracking/src/ui/routes/main_routes.dart';
+// import 'package:timetracking/src/ui/routes/main_routes.dart';
+import 'package:timetracking/src//utils/global.dart' as global;
 import 'package:timetracking/src/ui/screens/login.dart';
+import 'package:timetracking/src/utils/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MultiBlocProvider(providers: [
+    BlocProvider(create: (context) {
+      return LoginBloc();
+    }),
+  ], child: App()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+Future<bool> _loadCredentials() async {
+  final auth = await MySharedPreferences().get("AUTH");
+
+  if (auth == null) {
+    return (false);
+  }
+
+  return (true);
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      global.app = await Firebase.initializeApp();
+      global.auth = FirebaseAuth.instanceFor(app: global.app);
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'timetracking',
-      theme: ThemeData(
-        brightness: Brightness.light,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      themeMode: ThemeMode.system,
-      home: BlocProvider(
-        create: (_) => LoginBloc(),
-        child: const LoginScreen(),
-      ),
+    // Show error message if initialization failed
+    if (_error) {
+      return const MaterialApp(home: Text("Error"));
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return const MaterialApp(home: CircularProgressIndicator());
+    }
+
+    return FutureBuilder(
+      future: _loadCredentials(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            initialRoute: (snapshot.data == true ? '/home' : '/'),
+            title: 'TimeTracking',
+            theme: ThemeData(
+              brightness: Brightness.light,
+            ),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+            ),
+            home: BlocProvider(
+              create: (_) => LoginBloc(),
+              child: const LoginScreen(),
+            ),
+          );
+        } else {
+          return const MaterialApp(
+              home: Center(child: CircularProgressIndicator()));
+        }
+      },
     );
   }
 }

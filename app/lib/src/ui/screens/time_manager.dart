04681f5b46dfa694f59a59dfa75b46cb9191b73e
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-12-09 13:59:30 +0100

    Add basic screen

---
+++ app/lib/src/ui/screens/time_manager.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:timetracking/src/blocs/time_manager/bloc.dart';
+import 'package:timetracking/src/ui/widgets/time_manager_guest.dart';
+import 'package:timetracking/src/ui/widgets/time_manager_loading.dart';
+import 'package:timetracking/src/ui/widgets/time_manager_logged.dart';
+
+class TimeManagerScreen extends StatefulWidget {
+  const TimeManagerScreen({Key? key}) : super(key: key);
+
+  @override
+  _TimeManagerScreenState createState() => _TimeManagerScreenState();
+}
+
+class _TimeManagerScreenState extends State<TimeManagerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: BlocListener<TimeManagerBloc, TimeManagerState>(
+        listener: (context, state) async {},
+        child: BlocBuilder<TimeManagerBloc, TimeManagerState>(
+            buildWhen: (TimeManagerState previous, TimeManagerState current) {
+          return (true);
+        }, builder: (context, state) {
+          if (state is TimeManagerGuest) {
+            return (const TimeManagerGuestPage());
+          } else if (state is TimeManagerLoggedIn) {
+            return (const TimeManagerLoggedInPage());
+          } else if (state is TimeManagerLoading) {
+            return (const TimeManagerLoadingPage());
+          } else {
+            return (Container());
+          }
+        }),
+      ),
+    );
+  }
+}

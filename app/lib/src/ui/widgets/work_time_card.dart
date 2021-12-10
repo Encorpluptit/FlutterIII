cdd40b880a9e8927bdf8c7be1f0774a74ff01e21
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-12-10 11:49:45 +0100

    Add information about worked times

--- app/lib/src/ui/widgets/work_time_card.dart
+++ app/lib/src/ui/widgets/work_time_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:timetracking/src/models/work_time.dart';
 
 class WorkTimeCard extends StatelessWidget {
@@ -7,29 +8,58 @@ class WorkTimeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String clockIn =
+        DateFormat('dd/MM/yyy – HH:mm').format(workTime.in_!.toDate());
+    String clockOut = "Not yet clocked out";
+    if (workTime.out != null) {
+      clockOut = DateFormat('dd/MM/yyy – HH:mm').format(workTime.out!.toDate());
+    }
+    String approval = "Approved✅";
+    TextStyle approvalStyle = const TextStyle(color: Colors.green);
+    if (workTime.requiresApproval!) {
+      approval = "Not approved yet❌";
+      approvalStyle = const TextStyle(color: Colors.red);
+    }
     return Material(
-        child: Padding(
-      padding: EdgeInsets.all(10.0),
-      child: Ink(
-        width: 300,
-        height: 150,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: const Border(
-            top: BorderSide(width: 2.5, color: Colors.grey),
-            bottom: BorderSide(width: 2.5, color: Colors.grey),
-            left: BorderSide(width: 2.5, color: Colors.grey),
-            right: BorderSide(width: 2.5, color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Ink(
+            width: 300,
+            height: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: const Border(
+                top: BorderSide(width: 2.5, color: Colors.grey),
+                bottom: BorderSide(width: 2.5, color: Colors.grey),
+                left: BorderSide(width: 2.5, color: Colors.grey),
+                right: BorderSide(width: 2.5, color: Colors.grey),
+              ),
+            ),
+            child: InkWell(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text("Clocked in at: $clockIn"),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text("Clocked out at: $clockOut"),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    approval,
+                    style: approvalStyle,
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
-        child: const InkWell(
-          customBorder: CircleBorder(),
-          // child: Center(
-          //   child: Text(textValue, style: textStyle),
-          // ),
-          // splashColor: splashColor,
-        ),
       ),
-    ));
+    );
   }
 }

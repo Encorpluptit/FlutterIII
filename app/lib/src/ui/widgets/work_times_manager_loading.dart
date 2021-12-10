5c71ebcd60ab1f0c125ba3ca726cb983aad87472
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-12-10 12:06:52 +0100

    remove useless import

--- app/lib/src/ui/widgets/work_times_manager_loading.dart
+++ app/lib/src/ui/widgets/work_times_manager_loading.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:timetracking/src/blocs/work_times_manager/bloc.dart';
 import 'package:timetracking/src/utils/shared_preferences.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:timetracking/src/utils/global.dart' as global;
 
 class WorkTimesManagerLoadingPage extends StatefulWidget {
   const WorkTimesManagerLoadingPage({Key? key}) : super(key: key);

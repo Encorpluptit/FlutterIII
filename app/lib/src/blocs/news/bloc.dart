c92013589654e6cbddee1391b7fe16120ed1b640
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-12-11 15:58:18 +0100

    Fix data fetching

--- app/lib/src/blocs/news/bloc.dart
+++ app/lib/src/blocs/news/bloc.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:timetracking/src/models/news.dart';
-import 'package:timetracking/src/utils/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:timetracking/src/utils/global.dart' as global;
@@ -11,19 +10,14 @@ part 'state.dart';
 
 class NewsBloc extends Bloc<NewsEvent, NewsState> {
   final NewsState initialState;
-  NewsBloc([this.initialState = const NewsLoading()])
-  : super(initialState) {
+  NewsBloc([this.initialState = const NewsLoading()]) : super(initialState) {
     on<NewsLoadErrorEvent>(
         (event, emit) => emit(NewsLoadedFailure(event.error)));
-    on<NewsLoadEvent>(
-        (event, emit) async => emit(await _newsRequest(event)));
-
+    on<NewsLoadEvent>((event, emit) async => emit(await _newsRequest(event)));
   }
 
-  Future<NewsState> _newsRequest(
-      NewsEvent event) async {
+  Future<NewsState> _newsRequest(NewsEvent event) async {
     try {
-      /* final String userId = await MySharedPreferences().get("AUTH"); */
       List<NewsData> newsData = [];
       var resp = await global.store
           .collection("news")

fc1edce7dc78893fab8a9e0c5140312d7058acac
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-12-11 18:49:01 +0100

    Add IDE helper

--- app/lib/src/ui/widgets/work_time_edit_dialog.dart
+++ app/lib/src/ui/widgets/work_time_edit_dialog.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:timetracking/src/models/work_time.dart';
 
+// ignore: must_be_immutable
 class WorkTimeEditDialog extends StatefulWidget {
   final WorkTime workTime;
   final Function(DateTime in_, DateTime? out) onSave;
